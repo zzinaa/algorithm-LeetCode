5816ae14cdd517ddec256c585ca10330e489e6cd
Jinah Hwang <wlsdk2769@gmail.com>
2024-12-04 00:53:30 +0900

    Time: 1 ms (99.79%) | Memory: 61.4 MB (81.26%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+        int min = prices[0];
+
+        for(int i = 1; i < prices.length; i ++) {
+            if(prices[i] < min) {
+                min = prices[i];
+                continue;
+            }
+
+            profit = Math.max(profit, prices[i] - min);
+        }
+        
+        return profit;
+    }
+}
