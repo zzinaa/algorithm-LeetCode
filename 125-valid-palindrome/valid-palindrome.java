7f01476078c36140bbf4875e25184bfadfdfe502
Jinah Hwang <wlsdk2769@gmail.com>
2025-07-12 21:31:26 +0900

    Time: 5 ms (48.51%) | Memory: 44.2 MB (48.35%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        String str = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        String lowerS = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
 
-        if(str.isEmpty()) return true;
+        for(char c : lowerS.toCharArray()) {
+            if(Character.isDigit(c) || Character.isAlphabetic(c)) {
+                sb.append(c);
+            }
+        }
+
+        String palindromTarget = sb.toString();
 
         int start = 0;
-        int end = str.length() - 1;
+        int end = palindromTarget.length() - 1;
 
-        while(start <= end) {
-            // if(start >= str.length() || end < 0) break;
-            if(str.charAt(start) != str.charAt(end)) return false;
+        while(start < end) {
+            if(palindromTarget.charAt(start) != palindromTarget.charAt(end)) {
+                return false;
+            }
+
             start ++;
             end --;
         }
